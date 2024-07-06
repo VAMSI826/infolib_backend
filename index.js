66c540d84cc2cb26aeb0f05abaa6f40adb4f0e47
Vamsi826 <vamsim628@gmail.com>
2024-07-06 14:49:09 +0530

    2

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Import routes
 import userRoute from "./route/user.route.js";
 import bookrouter from "./route/books.route.js";
 import Adminrouter from "./route/admin.route.js";
@@ -12,37 +16,28 @@ import slotrouter from "./route/slot.route.js";
 import borrowedrouter from "./route/borrowed.route.js";
 import notifrouter from "./route/notification.route.js";
 import authroute from "./route/auth.route.js";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-// Define __dirname in ES module scope
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 
+dotenv.config();
+
 app.use(
-  cors()
-  //{
-  //   origin: ["https://infolib.vercel.app/"],
-  //   methods: ["GET", "POST", "DELETE"],
-  //   credentials: true,
-  // }
+  cors({
+    origin: ["https://infolib.vercel.app/"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow cookies to be sent with requests
+  })
 );
 app.use(express.json());
 app.use(bodyParser.json());
 
-dotenv.config();
-
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
-try {
-  mongoose.connect(URI);
-  console.log("Connected to MongoDB");
-} catch (error) {
-  console.log("Error:", error);
-}
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log("Error:", error));
 
 app.use("/user", userRoute);
 app.use("/book", bookrouter);
@@ -53,14 +48,6 @@ app.use("/borrowed", borrowedrouter);
 app.use("/notif", notifrouter);
 app.use("/api/auth", authroute);
 
-// app.get("/", (req, res) => {
-//   app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-//   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-// });
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 app.listen(PORT, () => {
-  console.log(` sever is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
