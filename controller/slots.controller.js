e2f6dc323319aac1330f1e5c302864eccedab909
Vamsi826 <vamsim628@gmail.com>
2024-07-07 21:42:38 +0530

    123444

--- controller/slots.controller.js
+++ controller/slots.controller.js
@@ -23,7 +23,11 @@ export const addSlot = async (req, res) => {
         barcode: barcode,
         title: book.title,
         borroweddate: date.toLocaleDateString(),
-        timeofbooking: date.toLocaleTimeString(),
+        timeofbooking: date.toLocaleTimeString("en-GB", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        }),
       });
       book.copies = book.copies - 1;
       console.log(book.copies);
